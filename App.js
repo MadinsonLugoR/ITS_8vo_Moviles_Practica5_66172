19469c32e4fcd728a4e851e81e2006f4c1c79c9a
MadinsonLugoR <159492561+MadinsonLugoR@users.noreply.github.com>
2024-05-09 09:57:36 -0600

    Primer Commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <LinearGradient colors={['steelblue','skyblue']} style={styles.container}>
+      <Text style={styles.Titulo}>Practica App React :D</Text>
+      <Text style={styles.Subtitulo}>Madinson Lugo</Text>
+      <TextInput placeholder='@gmail.com' style={styles.Login}/>
+      <TextInput placeholder='password' style={styles.Login}/>
       <StatusBar style="auto" />
-    </View>
+      <TouchableOpacity>
+        <LinearGradient colors={['#0099FF','#00FFFF']} style={styles.Boton}>
+            <Text style={{fontSize:15,fontWeight: 'bold', color:'white'}}>Aceptar</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'steelblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  Titulo: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  Subtitulo: {
+    color: 'white',
+    fontSize: 15,
+    fontStyle: 'italic',
+  },
+
+Login: {
+  color : 'white',
+  fontStyle : 'italic',
+  borderWidth : 1,
+  borderColor : 'white',
+  padding: 10,
+  paddingStart : 20,
+  width : '80%',
+  height : 50,
+  marginTop : 20,
+  borderRadius : 30,
+},
+
+Boton: {
+  padding: 15,
+  marginTop : 40,
+  borderColor : 'white',
+  borderWidth : 3,
+  borderRadius : 30,
+}
+
 });
